519893be35c8fedc39a9172cfb5776692aa282a0
Tu-Shin <maisha.tushin@northsouth.edu>
2025-07-14 09:42:21 +0600

    new rival heading for all products

--- components/home/Heading.jsx
+++ components/home/Heading.jsx
@@ -2,13 +2,15 @@ import { Ionicons } from "@expo/vector-icons";
 import { Text, TouchableOpacity, View } from 'react-native';
 import { COLORS } from "../../constants";
 import styles from './Heading.style';
-r
+
+
+
 const Heading = () => {
   return (
     <View style={styles.container}>
         <View style={styles.header}>
             <Text style={styles.headerTitle}>
-                Something
+                All Products
             </Text>
             <TouchableOpacity>
                 <Ionicons name="ios-grid" size={24} color={COLORS.primary}/>
--- components/home/Heading.style.js
+++ components/home/Heading.style.js
@@ -1,4 +1,22 @@
 import { StyleSheet } from "react-native";
+import { COLORS, SIZES } from "../../constants";
+const styles = StyleSheet.create({
 
-const styles = StyleSheet.create({})
+    container:{
+      marginTop : SIZES.medium,
+      marginBottom: SIZES.medium,
+      marginHorizontal:12
+    },
+    header:{
+        flexDirection:"row",
+        justifyContent:"space-between",
+        alignItems:"center",
+        marginBottom:SIZES.medium
+    },
+    headerTitle:{
+        fontSize:SIZES.large,
+        fontWeight:"bold",
+        color:COLORS.primary
+    },
+})
 export default styles;
