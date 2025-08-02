50708be7c5353f3c278a268d06a82fe2f1b62a5e
mahpara271 <emmasheikhmahpara@gmail.com>
2025-08-02 21:37:02 +0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ const UserSchema=new mongoose.Schema({
 username: {type: String,require: true},
 email: {type: String,require: true,unique: true},
 password: {type: String,require: true},
-location: {type: String,default: "Dhaka Bangladesh"},
+location: {type: String,require:true},
 },{timestamps:true});
+
 
 module.exports = mongoose.model("User", UserSchema)
